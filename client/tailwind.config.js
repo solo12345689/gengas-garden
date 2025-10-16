7514f2369f2c7074fd97eff593f2709af777ec1b
solo12345689 <sarthak.hasija.10095@kiitworld.in>
2025-10-16 22:28:43 +0530

    Format tailwind.config.js for better readability

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,1 +1,10 @@
-export default {content:['./index.html','./src/**/*.{js,jsx,ts,tsx}'],theme:{extend:{}},plugins:[]}
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
